5903089fea3950772571a6598cf42cc908410a3f
ThaiNguyennd <thaittn01@gmail.com>
2023-04-13 21:02:59 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,57 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import SignInPage from "./pages/SignInPage";
+import DashBoadPage from "./pages/DashBoadPage";
+import CamPaignPage from "./pages/CamPaignPage";
+import AddNewCampaign from "./pages/AddNewCampaign";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<DashBoadPage></DashBoadPage>}></Route>
+      <Route path="/sign-up" element={<SignUpPage></SignUpPage>}></Route>
+      <Route path="/sign-in" element={<SignInPage></SignInPage>}></Route>
+      <Route path="/campaign" element={<CamPaignPage></CamPaignPage>}></Route>
+      <Route
+        path="/start-campaign"
+        element={<AddNewCampaign></AddNewCampaign>}
+      ></Route>
+    </Routes>
   );
 }
 
